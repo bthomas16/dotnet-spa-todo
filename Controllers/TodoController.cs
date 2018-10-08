28cbe17f8b550074ded2ce3409a2217c953d4972
Brent <brentthomas.c@gmail.com>
2018-10-08 10:32:19 -0600

    'firstreal'

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using todo_vue.Models;
+
+namespace todo_vue.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        private static ConcurrentBag<Todo> todos = new ConcurrentBag<Todo> {
+        new Todo { Id = Guid.NewGuid(), Description = "Learn Vue" }
+    };
+
+        [HttpGet()]
+        public IEnumerable<Todo> GetTodos()
+        {
+            return todos.Where(t => !t.IsDone);
+        }
+
+        [HttpPost()]
+        public Todo AddTodo([FromBody]Todo todo)
+        {
+            todo.Id = Guid.NewGuid();
+            todo.IsDone = false;
+            todos.Add(todo);
+            return todo;
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult CompleteTodo(Guid id)
+        {
+            var todo = todos.SingleOrDefault(t => t.Id == id);
+            if (todo == null) return NotFound();
+
+            todo.IsDone = true;
+            return StatusCode(204);
+        }
+    }
+}
---
+++ Models/Todo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace todo_vue.Models
+{
+    public class Todo
+    {
+    public Guid Id { get; set; }
+    public string Description { get; set; }
+    public bool IsDone { get; set; }
+
+    }
+}
